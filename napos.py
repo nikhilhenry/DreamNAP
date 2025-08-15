96577dca7f2b4e044990080f472d16bf6ace916c
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2025-08-15 10:12:02 +0530

    create vertical menu

--- napos.py
+++ napos.py
@@ -1,17 +1,19 @@
 class NAPOperatingSystem:
     def __init__(self, driver):
         self.driver = driver
-        self.y_coord = 5
+        self.selected_index = 1
+        self.x_coord = 25 
 
     def step(self,keypressed,gyro_data):
         """
         The main loop of the driver.
         """
-        self.driver.blit("blue_square", 5, self.y_coord)
-        self.driver.blit("red_square", 25, self.y_coord)
-        self.driver.blit("blue_square", 45, self.y_coord)
+        self.driver.blit("blue_square" if self.selected_index == 0 else "red_square", self.x_coord,15)
+        self.driver.blit("blue_square" if self.selected_index == 1 else "red_square", self.x_coord,35)
+        self.driver.blit("blue_square" if self.selected_index == 2 else "red_square",self.x_coord,55)
 
         if keypressed[2]:
-            self.y_coord += 1
+            self.selected_index += 1 
         if keypressed[0]:
-            self.y_coord -= 1
+            self.selected_index -= 1
+        self.selected_index %= 3
